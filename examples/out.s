485d658e768f4a0795b2339404d112ddc9d6dc06
uan <fspinardi08@gmail.com>
2026-02-28 23:30:15 +0100

    -

--- examples/out.s
+++ examples/out.s
@@ -1,44 +1,26 @@
 .data
 .balign 8
 glb_1:
-	.ascii "%s %d\n"
+	.ascii "uan"
 	.byte 0
 /* end data */
 
 .data
 .balign 8
 glb_2:
-	.ascii "Hello, World!"
+	.ascii "Hello, %s!\n"
 	.byte 0
 /* end data */
-
-.text
-foo:
-	pushq %rbp
-	movq %rsp, %rbp
-	movq (%rdi), %rax
-	movq (%rax), %rsi
-	movq 8(%rax), %rdx
-	leaq glb_1(%rip), %rdi
-	callq printf
-	leave
-	ret
-.type foo, @function
-.size foo, .-foo
-/* end function foo */
 
 .text
 .globl main
 main:
 	pushq %rbp
 	movq %rsp, %rbp
-	subq $16, %rsp
-	leaq glb_2(%rip), %rax
-	movq %rax, -16(%rbp)
-	movq $13, -8(%rbp)
-	leaq -16(%rbp), %rdi
-	callq foo
-	movl $0, %eax
+	leaq glb_1(%rip), %rsi
+	leaq glb_2(%rip), %rdi
+	callq printf
+	movl $3, %eax
 	leave
 	ret
 .type main, @function
